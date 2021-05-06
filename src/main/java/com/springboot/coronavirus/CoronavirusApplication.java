c6bc94cbd259284816aa0d43e98d6145eaeeee9b
Lakshya Porwal <guplakshya@gmail.com>
2021-05-06 08:09:35 +0530

    src files

---
+++ src/main/java/com/springboot/coronavirus/CoronavirusApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.coronavirus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronavirusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronavirusApplication.class, args);
+	}
+}
